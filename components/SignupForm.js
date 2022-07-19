b20336709171e51abe70690414639046fe73a964
shahin shahi <shahin.shahi1986@gmail.com>
2022-07-19 16:30:57 +0300

    signup page

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,10 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PhoneInput from 'react-phone-number-input'
+import Select from 'react-select'
+import UploadFile from './UploadFile'
 import axios from 'axios'
 
 
 
 function SignupForm() {
+
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate', id: '1' },
+    { value: 'strawberry', label: 'Strawberry', id: '2' },
+    { value: 'vanilla', label: 'Vanilla', id: '3' }
+  ]
+
 
   const url = 'https://publicapi.dev.rently.sg/singpasstoken/a_FgbP52KimMeDntMy_bLXwY6AVUiDXHWwRPk9aOhe0'
 
@@ -13,14 +23,15 @@ function SignupForm() {
   const [data, setData] = useState({
     email: "",
     name: "",
-    number: ""
+    number: "",
+    country: ""
   })
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value })
   }
-  const handelNumberChange = (e) =>{
-    setData({...data, number: value})
+  const handelNumberChange = (e) => {
+    setData({ ...data, number: value })
   }
 
   const handleSubmit = (e) => {
@@ -102,7 +113,71 @@ function SignupForm() {
            block w-small rounded-md sm:text-sm focus:ring rounded-2xl"
               placeholder="tarik@yahoo.com" />
           </label>
-          <button type='submit' >Sign up</button>
+          <label className="block text-purple ">
+            <span className="after:ml-0.5
+           after:text-red-500 
+           block text-sm font-medium text-slate-700 px-1 py-3">
+              Residential status
+            </span>
+            <button className='bg-dark-gray text-purple w-[65px]
+           h-[40px] rounded-md'>Citizen</button>
+            <button className='bg-dark-gray text-purple w-[146px]
+           h-[40px] ml-5 rounded-md'>Permanent resident</button>
+            <button className='bg-dark-gray text-purple w-[65px]
+           h-[40px] ml-5 rounded-md'>Foreigner</button>
+          </label>
+          <label className='block text-purple'>
+            <span className="after:ml-0.5
+           after:text-red-500 
+           block text-sm font-medium text-slate-700 px-1 py-3">
+              Nationality
+            </span>
+            <Select
+              value={data.country}
+              onChange={handleChange}
+              styles={{ border: 'none' }}
+              className="mt-1 px-3 py-2 h-[53px]  border
+       shadow-sm  placeholder-slate-400 
+       focus:outline-none border-green focus:ring-purple
+        block w-small rounded-md sm:text-sm focus:ring rounded-2xl"
+              options={options} />
+          </label>
+          <label className="block text-purple ">
+            <span className="after:ml-0.5
+           after:text-red-500 
+           block text-sm font-medium text-slate-700 px-1 py-3">
+              NRIC number
+            </span>
+            <input type="number" name="number"
+              className="mt-1 px-3 py-2 h-[53px]  border
+          shadow-sm  placeholder-slate-400 
+          focus:outline-none border-green focus:ring-purple
+           block w-small rounded-md sm:text-sm focus:ring rounded-2xl"
+              placeholder="E.g. M3344456N"
+              value={data.number}
+              onChange={handleChange} />
+          </label>
+
+          <label className="block text-purple ">
+            <span className="after:ml-0.5
+           after:text-red-500 
+           block text-sm font-medium text-slate-700 px-1 py-3">
+              Type of property
+            </span>
+            <button className='bg-dark-gray text-purple w-[65px] h-[40px] rounded-md'>HDB</button>
+            <button className='bg-dark-gray text-purple w-[146px] h-[40px] ml-5 rounded-md'>Private residential</button>
+          </label>
+          <label className="block text-purple ">
+            <span className="after:ml-0.5
+           after:text-red-500 
+           block text-sm font-medium text-slate-700 px-1 py-3">
+              Proof of ownership
+            </span>
+            <span>You can login here at INLIS portal, take a photo that clearly<br></br>
+              shows your address and upload it here.</span>
+          </label>
+          <UploadFile />
+          <button type='submit'>Sign up</button>
         </form>
       </div>
     </div>
--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Nationality from '../components/Nationality'
+
 import SignupForm from '../components/SignupForm'
 
 function signup() {
   return (
     <div>
       <SignupForm />
-      <Nationality />
+    
     </div>
   )
 }
